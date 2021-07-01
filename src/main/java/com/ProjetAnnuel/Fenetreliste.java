f7ac8f191b644ee38a10d2ac34067777883a09a6
delattre <pierrejo74@gmail.com>
2021-07-01 20:24:13 +0200

    commit

--- src/main/java/com/ProjetAnnuel/Fenetreliste.java
+++ src/main/java/com/ProjetAnnuel/Fenetreliste.java
@@ -28,6 +28,7 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.TextArea;
 
 public class Fenetreliste extends javax.swing.JInternalFrame {
 
